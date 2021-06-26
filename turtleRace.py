2c45857fad39b13ec65de3b1030539eeb241ed94
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-06-26 15:40:38 -0400

    commiting cahing

--- turtleRace.py
+++ turtleRace.py
@@ -2,13 +2,11 @@ from random import *
 from setUp import *
 from racers import *
 import turtle
+winningXcor = 10
 def move(racer): 
     racer.forward(randint(1, 5))
-
 i = 0
-winningXcor = 10
-pen.goto(-80, -10)
-pen.color("white")
+pen = turtle.Turtle()
 for i in range(75):
     i = 0
     move(tom)
@@ -21,27 +19,21 @@ for i in range(75):
         pen.pendown()
         if tom.xcor() == winningXcor:
             pen.write("THE RED TURTLE FINISHED!")
-            pen.ycor(-10)
             break
         elif tommy.xcor() == winningXcor:
             pen.write("THE ORANGE TURTLE FINISHED!")
-            pen.ycor(-10)
             break
         elif bob.xcor() == winningXcor:
             pen.write("THE YELLOW TURTLE FINISHED!")
-            pen.ycor(-10)
             break
         elif bobby.xcor() == winningXcor:
             pen.write("THE GREEN TURTLE FINISHED!")
-            pen.ycor(-10)
             break
         elif jim.xcor() == winningXcor:
             pen.write("THE BLUE TURTLE FINISHED!")
-            pen.ycor(-10)
             break
         elif jimmy.xcor() == winningXcor:
             pen.write("THE PURPLE TURTLE FINISHED!")
-            pen.ycor(-10)
             break
         i+=1
 turtle.done()
